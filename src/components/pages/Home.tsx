c24e13e072f923c20fe97d199abebe0a4c088267
Bryan Astacio <brya0196@gmail.com>
2020-12-11 21:22:09 -0400

    refactor app structure

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
+import ExploreContainer from '../atoms/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
