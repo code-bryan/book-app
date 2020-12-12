06d13422d36914c86ee7862eba14e8892281cd39
Bryan Astacio <brya0196@gmail.com>
2020-12-12 16:04:11 -0400

    fixed back button

--- src/components/molecules/BackButton.tsx
+++ src/components/molecules/BackButton.tsx
@@ -6,6 +6,9 @@ import Colors from 'theme/Colors';
 
 const BackButtonIonic = styled(IonBackButton)`
     --color: ${Colors.SECONDARY};
+    font-size: 14px;
+    font-weight: 500;
+    margin-left: 10px;
 `;
 
 const BackButton: React.FC = () => {
