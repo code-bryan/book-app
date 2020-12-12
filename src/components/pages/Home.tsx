2bcdffc1dbf1ef63d573b8c755a4115cd7cafa66
Bryan Astacio <brya0196@gmail.com>
2020-12-11 21:58:34 -0400

    added missing templates

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,7 +1,5 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
-import ExploreContainer from '../atoms/ExploreContainer';
-import './Home.css';
 
 const Home: React.FC = () => {
   return (
@@ -17,7 +15,6 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
       </IonContent>
     </IonPage>
   );
